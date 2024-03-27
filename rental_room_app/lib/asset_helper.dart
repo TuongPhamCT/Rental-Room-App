d84ffad34d1e26a109cb040db6169912d4e71cc6
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-03-27 13:19:48 +0700

    first code

---
+++ rental_room_app/lib/asset_helper.dart
@@ -0,0 +1,9 @@
+class AssetHelper {
+  //Assets in icons
+  static const String iconAssets = 'assets/icons/';
+  //Assets in images
+  static const String imgAssets = 'assets/images/';
+
+  //splash
+  static const String logo1 = imgAssets + 'logo.png';
+}
