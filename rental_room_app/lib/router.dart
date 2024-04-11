1029c73363a0c13bed7ddb2ba4784aa694f37bf0
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-04-11 23:52:19 +0700

    receipt detail UI done

--- rental_room_app/lib/router.dart
+++ rental_room_app/lib/router.dart
@@ -11,46 +11,46 @@ import 'package:rental_room_app/Views/signup_screen.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
+    // GoRoute(
+    //   path: '/',
+    //   redirect: (BuildContext context, GoRouterState state) {
+    //     if (FirebaseAuth.instance.currentUser != null) {
+    //       return '/home';
+    //     } else {
+    //       return '/log_in';
+    //     }
+    //   },
+    // ),
+    // GoRoute(
+    //     path: '/log_in',
+    //     name: 'log_in',
+    //     builder: (context, state) => const LoginScreen(key: Key('log_in')),
+    //     routes: [
+    //       GoRoute(
+    //           path: 'sign_up',
+    //           name: 'sign_up',
+    //           builder: (context, state) =>
+    //               const SignupScreen(key: Key('sign_up')),
+    //           routes: [
+    //             GoRoute(
+    //                 path: 'pincode/:email',
+    //                 name: 'pincode',
+    //                 builder: (context, state) => PincodeScreen(
+    //                     key: const Key('pincode'),
+    //                     email: state.pathParameters['email']),
+    //                 routes: [
+    //                   GoRoute(
+    //                     path: 'register_form',
+    //                     name: 'register_form',
+    //                     builder: (context, state) => RegisterFormScreen(
+    //                         key: const Key('register_form'),
+    //                         email: state.pathParameters['email']),
+    //                   )
+    //                 ])
+    //           ]),
+    //     ]),
     GoRoute(
-      path: '/',
-      redirect: (BuildContext context, GoRouterState state) {
-        if (FirebaseAuth.instance.currentUser != null) {
-          return '/home';
-        } else {
-          return '/log_in';
-        }
-      },
-    ),
-    GoRoute(
-        path: '/log_in',
-        name: 'log_in',
-        builder: (context, state) => const LoginScreen(key: Key('log_in')),
-        routes: [
-          GoRoute(
-              path: 'sign_up',
-              name: 'sign_up',
-              builder: (context, state) =>
-                  const SignupScreen(key: Key('sign_up')),
-              routes: [
-                GoRoute(
-                    path: 'pincode/:email',
-                    name: 'pincode',
-                    builder: (context, state) => PincodeScreen(
-                        key: const Key('pincode'),
-                        email: state.pathParameters['email']),
-                    routes: [
-                      GoRoute(
-                        path: 'register_form',
-                        name: 'register_form',
-                        builder: (context, state) => RegisterFormScreen(
-                            key: const Key('register_form'),
-                            email: state.pathParameters['email']),
-                      )
-                    ])
-              ]),
-        ]),
-    GoRoute(
-        path: '/home',
+        path: '/',
         name: 'home',
         builder: (context, state) => const BottomBar(key: Key('home')),
         routes: [
--- rental_room_app/lib/themes/text_styles.dart
+++ rental_room_app/lib/themes/text_styles.dart
@@ -2,74 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:rental_room_app/themes/color_palete.dart';
 
 extension ExtendedTextStyle on TextStyle {
-  TextStyle get light {
-    return copyWith(fontWeight: FontWeight.w300, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get regular {
-    return copyWith(fontWeight: FontWeight.w400, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get italic {
-    return copyWith(
-        fontWeight: FontWeight.normal,
-        fontStyle: FontStyle.italic,
-        fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get medium {
-    return copyWith(fontWeight: FontWeight.w500, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get fontHeader {
-    return copyWith(fontSize: 22, height: 22 / 20, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get fontCaption {
-    return copyWith(fontSize: 12, height: 12 / 10, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get semibold {
-    return copyWith(fontWeight: FontWeight.w600, fontFamily: AppFonts.rubik);
-  }
-
   TextStyle get bold {
     return copyWith(fontWeight: FontWeight.w700, fontFamily: AppFonts.rubik);
   }
 
-  TextStyle get blackText {
-    return copyWith(color: ColorPalette.blackText, fontFamily: AppFonts.rubik);
-  }
-
   TextStyle get grayText {
     return copyWith(color: ColorPalette.grayText, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get primaryTextColor {
-    return copyWith(
-        color: ColorPalette.primaryColor, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get darkPrimaryTextColor {
-    return copyWith(
-        color: ColorPalette.darkBlueText, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get whiteTextColor {
-    return copyWith(color: Colors.white, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle get subTitleTextColor {
-    return copyWith(color: ColorPalette.blackText, fontFamily: AppFonts.rubik);
-  }
-
-  // convenience functions
-  TextStyle setColor(Color color) {
-    return copyWith(color: color, fontFamily: AppFonts.rubik);
-  }
-
-  TextStyle setTextSize(double size) {
-    return copyWith(fontSize: size, fontFamily: AppFonts.rubik);
   }
 }
 
@@ -84,12 +22,7 @@ class TextStyles {
       fontWeight: FontWeight.w400,
       height: 16 / 14,
       fontFamily: AppFonts.rubik);
-  static const TextStyle subTextStyle = TextStyle(
-      fontSize: 12,
-      color: ColorPalette.blackText,
-      fontWeight: FontWeight.w400,
-      height: 16 / 14,
-      fontFamily: AppFonts.rubik);
+
   static const TextStyle h1 = TextStyle(
       fontSize: 30,
       color: ColorPalette.blackText,
@@ -260,6 +193,12 @@ class TextStyles {
     fontWeight: FontWeight.w300,
     fontSize: 12,
     color: ColorPalette.blackText,
+  );
+  static const TextStyle total = TextStyle(
+    fontFamily: AppFonts.inter,
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    color: ColorPalette.primaryColor,
   );
 }
 
